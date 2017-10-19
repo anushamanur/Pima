c97a71cf80290a178f27914eb973c186b5d11100
anushamanur <32357808+anushamanur@users.noreply.github.com>
2017-10-19 20:04:29 +0530

    Add files via upload

---
+++ pima_indian.py
@@ -0,0 +1,27 @@
+# Create your first MLP in Keras
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy
+from sklearn.model_selection import train_test_split
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load pima indians dataset
+dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
+
+(X_train, X_test, Y_train, Y_test) = train_test_split(X, Y, test_size=0.33, random_state=seed)
+# create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# Compile model
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+# Fit the model
+model.fit(X_train, Y_train, epochs=500, batch_size=10)
+# evaluate the model
+scores = model.evaluate(X_test, Y_test)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
